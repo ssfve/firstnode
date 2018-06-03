b862798c0b025e9478144295bbc4e1758f7ed10b
Elvin Lin <ssfvessfve@163.com>
2018-06-03 15:39:47 +0800

    123

--- app.js
+++ app.js
@@ -47,16 +47,13 @@ app.use(function(err, req, res, next) {
 });
 
 log4js.configure({
-    appenders: [
-        { type: 'console' }, //控制台输出
-        {
-            type: 'file', //文件输出
-            filename: 'logs/access.log',
-            maxLogSize: 1024,
-            backups:3,
-            category: 'normal'
-        }
-    ]
+    appenders:{
+        type: 'file', //文件输出
+        filename:'logs/access.log',
+        maxLogSize:1024,
+        backups:3,
+        category:'normal'
+    }
 });
 
 let logger = log4js.getLogger('normal');
