c895b50a5c5632f95a753f80f64738e5bea3fd32
Elvin Lin <ssfvessfve@163.com>
2018-06-03 15:45:56 +0800

    123

--- app.js
+++ app.js
@@ -63,15 +63,13 @@ log4js.configure({
     }
 });
 
-let logger = log4js.getLogger('normal');
-logger.setLevel('INFO');
+let logger = log4js.getLogger();
 //app.use(log4js.connectLogger(logger, {level:log4js.levels.INFO}));
-app.use(log4js.connectLogger(logger, {level:log4js.levels.INFO, format:':method :url'}));
+app.use(log4js.connectLogger(logger));
 app.use(app.router);
 
 exports.logger=function(name){
     let logger = log4js.getLogger(name);
-    logger.setLevel('INFO');
     return logger;
 };
 
