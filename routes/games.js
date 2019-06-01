ebc451c43dbc93d8fc2e58c5f585f6af421eb516
MrRedHerring <ssfvessfve@163.com>
2019-06-01 13:03:35 +0800

    Update games.js

--- routes/games.js
+++ routes/games.js
@@ -215,7 +215,7 @@ router.get('/savePDFInfo', function (req, res) {
     let params = URL.parse(req.url, true).query;
     client.query("use " + TEST_DATABASE);
     console.log(params.gstone_id);
-    let modSql = 'INSERT INTO upload_pdf_table (mod_no,source_detail,gstone_id)' +
+    let modSql = 'INSERT INTO upload_pdf_table (mode_no,source_detail,gstone_id)' +
         ' values (?,?,?)';
     let modSqlParams = [params.mod_name, params.source_detail, params.gstone_id];
 
