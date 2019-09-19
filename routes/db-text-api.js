1747cbb09e6a031afe9863a6d8a0a6705dd62e6e
MrRedHerring <ssfvessfve@163.com>
2019-09-19 07:24:26 +0000

    Update db-text-api.js

--- routes/db-text-api.js
+++ routes/db-text-api.js
@@ -29,34 +29,7 @@ router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
-module.exports = router;
-
-let getButtonInfo=function(req, res, next) {
-    let text = new Text();
-    let params = URL.parse(req.url, true).query;
-
-    //client.connect();
-    client.query("use " + TEST_DATABASE);
-    // create a default new page record with default background image of id 0
-    let modSql = 'Select button1_id,button2_id,button3_id,button4_id from raw_control_table where page_id=?';
-    let modSqlParams = [params.page_id];
-    let result = null;
-    client.query(modSql, modSqlParams,
-        function selectCb(err, results, fields) {
-            if (err) {
-                throw err;
-            }
-            if (results) {
-                result = results[0];
-                console.log(result);
-            }
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.send(JSON.stringify(result));
-        });
-};
-
 let createText=function(req, res, next) {
-    let text = new Text();
     let params = URL.parse(req.url, true).query;
 
     client.query("use " + TEST_DATABASE);
@@ -76,7 +49,6 @@ let createText=function(req, res, next) {
                 result = results[0]['LAST_INSERT_ID()'];
                 console.log(result);
             }
-            //res.setHeader("Access-Control-Allow-Origin", "*");
             res.locals.textid = result;
             next();
         });
@@ -87,7 +59,7 @@ let getTextAttribute=function(req, res, next) {
 
     client.query("use " + TEST_DATABASE);
     // create a default new page record with default background image of id 0
-    let modSql = 'select '+params.attribute_name+' from raw_text_table where text_id=?';
+    let modSql = 'select '+params.attribute_name+' from raw_text_table where textID=?';
     let modSqlParams = [params.text_id];
     let result = null;
     client.query(modSql, modSqlParams,
@@ -99,7 +71,6 @@ let getTextAttribute=function(req, res, next) {
                 result = results[0][params.attribute_name];
                 console.log(result);
             }
-            res.setHeader("Access-Control-Allow-Origin", "*");
             res.send(result.toString());
         });
 };
@@ -118,10 +89,11 @@ let saveTextToPage=function (req, res, next) {
             if (err) {
                 throw err;
             }
-            res.setHeader("Access-Control-Allow-Origin", "*");
             res.send(res.locals.textid.toString());
         });
 };
 
 router.get('/getTextAttribute', [getTextAttribute]);
 router.get('/writeTextDB', [createText, saveTextToPage]);
+
+module.exports = router;
