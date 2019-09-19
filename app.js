54138f6144f77ca895c47909edd2cbd09279b796
MrRedHerring <ssfvessfve@163.com>
2019-09-19 05:15:25 +0000

    add change

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/games', games);
--- routes/games.js
+++ routes/games.js
@@ -184,8 +184,8 @@ router.post('/savePDF', function (req, res) {
     let form = new formidable.IncomingForm();
     form.uploadDir = config.uploadDir;
     form.keepExtensions = true;
+
     form.parse(req, function (err, fields, files) {
-        res.setHeader("Access-Control-Allow-Origin", "*");
         res.send({status: "200", responseType: "String", response: "success"})
     });
 
