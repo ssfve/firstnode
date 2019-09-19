2aa07036343924389f4017350bda59d8cc17c49e
MrRedHerring <ssfvessfve@163.com>
2019-09-19 05:18:07 +0000

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Credentials', "true");
     next();
 });
 
