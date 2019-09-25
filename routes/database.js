2ce7ea91878652a0b0a4ad839b6d6a0bfba25795
MrRedHerring <ssfvessfve@163.com>
2019-09-25 06:04:17 +0000

    Update database.js

--- routes/database.js
+++ routes/database.js
@@ -204,7 +204,7 @@ let updateAttributeInner=function(req, res, next) {
             if (err) {
                 throw err;
             }
-            res.send(params.attribute_value.toString());
+            res.send(res.locals.attribute_value.toString());
         });
 };
 
