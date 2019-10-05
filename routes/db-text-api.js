90897362eec7fb3276f66dfbbfc8a94d8adb961c
MrRedHerring <ssfvessfve@163.com>
2019-10-05 16:13:24 +0800

    Update db-text-api.js

--- routes/db-text-api.js
+++ routes/db-text-api.js
@@ -97,8 +97,37 @@ let saveTextToPage=function (req, res, next) {
         });
 };
 
+let getPageTextContent=function(req, res, next) {
+    let params = URL.parse(req.url, true).query;
+
+    client.query("use " + TEST_DATABASE);
+    // create a default new page record with default background image of id 0
+    let modSql = 'select a.text_content ' +
+        'from raw_text_table as a,raw_control_table as b' +
+        'where a.textID = b.text1_id ' +
+        'and b.page_id =?';
+    let modSqlParams = [params.page_id];
+    let result = null;
+    client.query(modSql, modSqlParams,
+        function selectCb(err, results, fields) {
+            if (err) {
+                throw err;
+            }
+            if (results) {
+                if(results[0] !== undefined){
+                    result = results[0]['text_content'];
+                    console.log(result);
+                }else{
+                    result = ''
+                }
+            }
+            res.send(result.toString());
+        });
+};
+
 router.get('/getTextAttribute', [getTextAttribute]);
 router.get('/writeTextDB', [createText, saveTextToPage]);
+router.get('/getPageTextContent', [getPageTextContent]);
 
 module.exports = {
     router,
