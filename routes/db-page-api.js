7d55ce774f407cd53e872a98353ba4d9d0f51ef4
MrRedHerring <ssfvessfve@163.com>
2019-10-05 17:01:01 +0800

    Update db-page-api.js

--- routes/db-page-api.js
+++ routes/db-page-api.js
@@ -30,7 +30,7 @@ router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
-let writePageDB=function(req, res, next){
+let writePageDB = function (req, res, next) {
     let text = new Text();
     let params = URL.parse(req.url, true).query;
     //client.connect();
@@ -57,7 +57,7 @@ let writePageDB=function(req, res, next){
         });
 };
 
-let getGuideId=function(req, res, next) {
+let getGuideId = function (req, res, next) {
     let text = new Text();
     let params = URL.parse(req.url, true).query;
 
@@ -83,11 +83,11 @@ let getGuideId=function(req, res, next) {
 
 };
 
-let savePageAttribute=function (req, res, next) {
+let savePageAttribute = function (req, res, next) {
     let params = URL.parse(req.url, true).query;
     //client.connect();
     client.query("use " + TEST_DATABASE);
-    let modSql = 'Update raw_button_table set '+params.attribute_name+'=? where page_id =?';
+    let modSql = 'Update raw_button_table set ' + params.attribute_name + '=? where page_id =?';
     let modSqlParams = [params.page_id, params.attribute_value];
 
     client.query(modSql, modSqlParams,
@@ -100,12 +100,12 @@ let savePageAttribute=function (req, res, next) {
         });
 };
 
-let getPageAttribute=function(req, res, next) {
+let getPageAttribute = function (req, res, next) {
     let params = URL.parse(req.url, true).query;
 
     //client.connect();
     client.query("use " + TEST_DATABASE);
-    let modSql = 'select '+params.attribute_name+' from raw_control_table where page_id = ?';
+    let modSql = 'select ' + params.attribute_name + ' from raw_control_table where page_id = ?';
     let modSqlParams = [params.page_id];
     let result = '';
     client.query(modSql, modSqlParams,
@@ -117,17 +117,17 @@ let getPageAttribute=function(req, res, next) {
                 console.log(results);
                 result = results[0][params.attribute_name]
             } else {
-                console.log('there is no record of '+params.attribute_name);
+                console.log('there is no record of ' + params.attribute_name);
             }
-            if(result === null){
+            if (result === null) {
                 res.send(null);
-            }else{
+            } else {
                 res.send(result.toString());
             }
         });
 };
 
-let getButtonInfoFromPage=function(req, res, next) {
+let getButtonInfoFromPage = function (req, res, next) {
     let text = new Text();
     let params = URL.parse(req.url, true).query;
 
@@ -150,7 +150,7 @@ let getButtonInfoFromPage=function(req, res, next) {
         });
 };
 
-let getValidGuides=function (req, res, next) {
+let getValidGuides = function (req, res, next) {
     let params = URL.parse(req.url, true).query;
 
     client.query("use " + TEST_DATABASE);
@@ -166,7 +166,60 @@ let getValidGuides=function (req, res, next) {
         });
 };
 
-router.get('/getButtonInfo', [getButtonInfoFromPage]);
+let getPageButtonList = function (req, res, next) {
+    let text = new Text();
+    let params = URL.parse(req.url, true).query;
+
+    //client.connect();
+    client.query("use " + TEST_DATABASE);
+    // create a default new page record with default background image of id 0
+    let modSql = 'Select button1_id,' +
+        'button2_id,' +
+        'button3_id,' +
+        'button4_id from raw_control_table where page_id=?';
+    let modSqlParams = [params.page_id];
+    let result = null;
+    client.query(modSql, modSqlParams,
+        function selectCb(err, results, fields) {
+            if (err) {
+                throw err;
+            }
+            if (results) {
+                result = results[0];
+                console.log(result);
+                res.locals.result = result;
+                next();
+            }
+            res.send(JSON.stringify(result));
+        });
+};
+
+let getButtonText = function (req, res, next) {
+    let buttonList = res.locals.result;
+    for (let i in buttonList) {
+        let buttonName = 'button' + i + '_id';
+        let button_id = buttonList.buttonName;
+        let modSql = 'Select buttonText from raw_button_table where button_id=?';
+        let modSqlParams = [button_id];
+        let result = null;
+        client.query(modSql, modSqlParams,
+            function selectCb(err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+                if (results) {
+                    let button_key = 'button' + i + '_text';
+                    result = results[0]['button_text'];
+                    buttonList.button_key = result;
+                    console.log(buttonList);
+                }
+            });
+    }
+    res.send(JSON.stringify(buttonList));
+};
+
+router.get('/getButtonInfoFromPage', [getButtonInfoFromPage]);
+router.get('/getPageButtonList', [getPageButtonList, getButtonText]);
 router.get('/getPageAttribute', [getPageAttribute]);
 router.get('/createBranchPage', [writePageDB]);
 router.get('/getValidGuides', [getValidGuides]);
